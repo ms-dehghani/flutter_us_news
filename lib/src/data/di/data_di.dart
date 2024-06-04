4c826fab5f6c99902df78617f15d632134830a26
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-06-04 23:56:56 +0330

    Add di

--- lib/src/data/di/data_di.dart
+++ lib/src/data/di/data_di.dart
@@ -1,7 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_us_news/src/base/base_di.dart';
+import 'package:flutter_us_news/src/data/constants/constants.dart';
+import 'package:flutter_us_news/src/data/datasource/news/api/news_api_data_provider.dart';
+import 'package:flutter_us_news/src/data/datasource/news/db/news_db_data_provider.dart';
+import 'package:flutter_us_news/src/data/repositories/news/news_repository_impl.dart';
 import 'package:get_it/get_it.dart';
+import 'package:sqflite/sqflite.dart';
 
-class DataDI {
+class DataDI implements BaseDi {
   final getIt = GetIt.instance;
 
-  DataDI() {}
+  @override
+  Future<void> provideDependencies() async {
+    _provideDio();
+    await _provideDatabase();
+    _provideNewsRepository();
+  }
+
+  void _provideDio() {
+    var timeOut = const Duration(seconds: 10);
+    Dio dio = Dio(BaseOptions(
+        baseUrl: Constants.baseUrl,
+        sendTimeout: timeOut,
+        receiveTimeout: timeOut,
+        connectTimeout: timeOut));
+
+    getIt.registerSingleton<Dio>(dio);
+  }
+
+  Future<void> _provideDatabase() async {
+    var db = await openDatabase('my_db.db');
+    getIt.registerSingleton<Database>(db);
+  }
+
+  void _provideNewsRepository() {
+    var newsApiDataProvider = NewsApiDataProvider(getIt());
+    var newsDbDataProvider = NewsDbDataProvider(getIt());
+
+    getIt.registerSingleton<NewsApiDataProvider>(newsApiDataProvider);
+    getIt.registerSingleton<NewsDbDataProvider>(newsDbDataProvider);
+
+    var newsRepositoryImpl =
+        NewsRepositoryImpl(newsDbDataProvider, newsApiDataProvider);
+
+    getIt.registerSingleton<NewsRepositoryImpl>(newsRepositoryImpl);
+  }
 }
--- lib/src/domain/di/domain_di.dart
+++ lib/src/domain/di/domain_di.dart
@@ -1,7 +1,18 @@
+import 'package:flutter_us_news/src/base/base_di.dart';
+import 'package:flutter_us_news/src/data/repositories/news/news_repository_impl.dart';
+import 'package:flutter_us_news/src/domain/uscases/news/item/news_item_usecase.dart';
+import 'package:flutter_us_news/src/domain/uscases/news/list/news_list_usecase.dart';
 import 'package:get_it/get_it.dart';
 
-class DomainDI {
+class DomainDI implements BaseDi {
   final getIt = GetIt.instance;
 
-  DomainDI() {}
+  @override
+  Future<void> provideDependencies() async {
+    getIt.registerSingleton<NewsItemUseCase>(
+        NewsItemUseCase(repository: getIt<NewsRepositoryImpl>()));
+
+    getIt.registerSingleton<NewsListUseCase>(
+        NewsListUseCase(repository: getIt<NewsRepositoryImpl>()));
+  }
 }
