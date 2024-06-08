96d4abb42751805efc2c40d28806aac7fd0fedea
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-06-08 22:06:39 +0330

    Change sort type

--- lib/src/utils/time_util.dart
+++ lib/src/utils/time_util.dart
@@ -3,5 +3,5 @@ import 'package:flutter_us_news/src/utils/extensions/translates_string_extension
 
 String timeToText(int timestamp, {bool withDay = true}) {
   var date = DateTime.fromMillisecondsSinceEpoch(timestamp);
-  return "${Texts.monthName[date.month - 1].translate} ${withDay ? date.day : ""} ${date.year} ${date.hour}:${date.minute}";
+  return "${Texts.monthName[date.month - 1].translate} ${withDay ? date.day.toString().padLeft(2, '0') : ""} ${date.year} ${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}:${date.second.toString().padLeft(2, '0')}";
 }
